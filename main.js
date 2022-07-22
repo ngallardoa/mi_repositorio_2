7b34bd304addeffd3ce70d39c76e1f05242d23cb
Nicolas Gallardo <gallardo.3853@gmail.com>
2022-07-22 14:41:17 -0300

    Corrección 22/7/22

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-let cantidad;
-const precioPan = 200;
-const precioTorta = 400;
-const precioGalletitas = 250;
 let totalCompra;
-let producto1 = prompt("Bienvenido a Lazaretto Bakery. Por favor, elija el producto que desea comprar: 'a' pan por kilo x $200, 'b' porción de torta x $400, 'c' galletitas por unidad x $250, 'd' para cancelar");
 
+function pedirProducto(producto1, cantidad){
+    const precioPan = 200;
+    const precioTorta = 400;
+    const precioGalletitas = 250;
+    producto1 = prompt("Bienvenido a Lazaretto Bakery. Por favor, elija el producto que desea comprar: 'a' pan por kilo x $200, 'b' porción de torta x $400, 'c' galletitas por unidad x $250, 'd' para cancelar");
 
-while (producto1 != 'd'){
-    cantidad = parseInt (prompt("Ingrese la cantidad de unidades que quiere comprar"));
-    switch (producto1){
-        case 'a':
-            totalCompra = (precioPan * cantidad);
-            console.log ("El total a pagar es: $", totalCompra);
-            break;
+    while (producto1 != 'd'){
+        cantidad = parseInt (prompt("Ingrese la cantidad de unidades que quiere comprar"));
+        switch (producto1){
+            case 'a':
+                totalCompra = (precioPan * cantidad);
+                alert ("El total a pagar es: $" + totalCompra);
+                break;
+        
+            case 'b':
+                totalCompra = (precioTorta * cantidad);
+                alert ("El total a pagar es: $" + totalCompra);
+                break;
     
-        case 'b':
-            totalCompra = (precioTorta * cantidad);
-            console.log ("El total a pagar es: $", totalCompra);
-            break;
+            case 'c':
+                totalCompra = (precioGalletitas * cantidad);
+                alert ("El total a pagar es: $" + totalCompra);
+                break;
+    
+            default:
+                break;
+    
+        }
+        producto1 = prompt("Desea hacer algún pedido más? 'a' pan por kilo x $200, 'b' porción de torta x $400, 'c' galletitas por unidad x $250, 'd' para cancelar");
+    
+    } 
+}
 
-        case 'c':
-            totalCompra = (precioGalletitas * cantidad);
-            console.log ("El total a pagar es: $", totalCompra);
-            break;
-
-        default:
-            break;
-
-    }
-    producto1 = prompt("Desea hacer algún pedido más? 'a' pan por kilo x $200, 'b' porción de torta x $400, 'c' galletitas por unidad x $250, 'd' para cancelar");
-
-} 
-
-
+pedirProducto();
